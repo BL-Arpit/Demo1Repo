227de015ea1d25ca98de6180187f2522ec2a2688
unknown <arpit.sahu@bridgelabz.com>
2024-01-16 12:36:59 +0530

    Added : Substraction feature

--- file1.sh
+++ file1.sh
@@ -2,13 +2,16 @@ echo "Enter 1st number"
 read a
 echo "Enter 2nd Number"
 read b
-echo "Enter 1 for addition"
+echo "Enter 1 for addition, 2 for substraction"
 read input
 
 case $input in
 	
 	1) sum=$(($a+$b))
 	echo $sum
+	;;
+	2) sub=$(($a-$b))
+	echo $sub
 	;;
 	*) echo "*Invalid option"
 	;;
